f29a9e91c9e1f6eb85d8ed700a6f5a6f427303e1
Alexander Schelchere <ascellie@gmail.com>
2019-07-31 16:38:44 -0400

    main(routines adapter): initial setup, make calls to api

--- js/main/Routines/RoutinesAdapter.js
+++ js/main/Routines/RoutinesAdapter.js
@@ -1,18 +1,28 @@
 class RoutinesAdapter {
   // Get the url endpoint for routines
-  static baseUrl() {
+  static url() {
     return 'http://localhost:3000/routines/';
+  }
+
+  // GET all routines
+  static async getRoutines() {
+    const res = await fetch(this.url());
+    return await res.json();
   }
 
   // set headers for requests
   static getHeaders() {
     return {
       'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      Accept: 'application/json'
     };
   }
 
   static fetchConfig(httpVerb, bodyObject) {
-    
+    return {
+      method: httpVerb,
+      headers: this.getHeaders(),
+      body: JSON.stringify(bodyObject)
+    };
   }
 }
