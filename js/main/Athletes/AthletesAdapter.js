08776b089e3eaf4f4cbc14e69df368e622acc050
Kevin Lai <kevinlai76@gmail.com>
2019-08-01 11:28:17 -0400

    athlete pages divs and elements

---
+++ js/main/Athletes/AthletesAdapter.js
@@ -0,0 +1,28 @@
+class AthletesAdapter {
+// Get the url endpoint for athletes
+  static url() {
+    return 'http://localhost:3000/athletes/';
+  }
+
+  // GET all athletes
+  static async getAthletes() {
+    const res = await fetch(this.url());
+    return await res.json();
+  }
+
+  // set headers for requests
+  static getHeaders() {
+    return {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    };
+  }
+
+  static fetchConfig(httpVerb, bodyObject) {
+    return {
+      method: httpVerb,
+      headers: this.getHeaders(),
+      body: JSON.stringify(bodyObject)
+    };
+  }
+}
--- js/main/AthletesPage.js
+++ js/main/AthletesPage.js
@@ -2,15 +2,20 @@ class AthletesPage {
   constructor() {
     // Set current page
     this.currentPage = document.querySelector('.athletes');
-    // Create controller for routines (This will hold the filter and "Create Routine" button)
-    this.routinesController = document.createElement('div');
-    // Create the container for the list of routines
-    this.routines = document.createElement('div');
+    // Create controller for athletes (This will hold the filter and "Create Routine" button)
+    this.athletesListContainer = document.createElement('div');
+    // Create the container for the list of athletes
+    this.athletes = document.createElement('div');
     // Append them to our main div
-    this.currentPage.append(this.routinesController);
-    this.currentPage.append(this.routines);
+    this.currentPage.append(this.athletesListContainer);
+    this.athletesListContainer.classList.add('athletes-list-container')
+    this.currentPage.append(this.athletes);
     // Add the inner HTML
-    this.routinesController.innerHTML = '<p>Athletes List Container</p>';
-    this.routines.innerHTML = '<p>Message Box</p>';
+    // this.athletesListContainer.innerHTML = '<p>Athletes List Container</p>';
+    this.athletes.innerHTML = '<p>Message Box</p>';
+    // athleteslistfilter here
+    this.athletesList = new AthletesList()
+    this.athletesFilter = new AthletesListFilter()
   }
 }
+
