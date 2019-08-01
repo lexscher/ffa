d4ab4877e0971caef656bc6beb0e01db5cdabd94
Alexander Schelchere <ascellie@gmail.com>
2019-08-01 14:35:27 -0400

    js(main): change state of the current user

--- js/main/landingpage.js
+++ js/main/landingpage.js
@@ -18,10 +18,10 @@ class LandingPage {
     this.loginPage.classList.add('login-page');
     this.registrationPage.classList.add('registration-page');
 
-    this.logInForm = new LoginForm()
-    console.log(this.logInForm)
-    this.registrationForm = new RegistrationForm()
-    console.log(this.registrationForm)
+    this.logInForm = new LoginForm();
+    console.log(this.logInForm);
+    this.registrationForm = new RegistrationForm();
+    console.log(this.registrationForm);
   }
 
   // toggle our state
@@ -47,7 +47,6 @@ class LandingPage {
     this.hideAllLandingPages();
     let currentPage = document.querySelector(`.${pageName}`);
     currentPage.classList.remove('div--hidden');
-    
   }
 }
 
@@ -57,31 +56,59 @@ class LoginForm {
     this.loginFormContainer = document.createElement('div');
     this.currentPage.append(this.loginFormContainer);
     this.loginFormContainer.classList.add('login-form-container');
-    this.loginFormContainer.innerHTML = 
-    `
+    this.loginFormContainer.innerHTML = `
     <form class="login-form" method="get">
       Username: <input type="text" name="username"><br>
       Password: <input type="text" name="password"><br>
       <input type="submit" value="Submit">
     </form>
-    `
-    this.loginForm = document.querySelector('.login-form')
+    `;
+    this.loginForm = document.querySelector('.login-form');
     this.loginForm.addEventListener('submit', event => {
-      event.preventDefault()
-      alert('Logging in')
-    })
-  
+      event.preventDefault();
+      this.loginValidator(event);
+    });
+  }
+
+  loginValidator(event) {
+    // set athlete's username & password
+    let username = event.target.username.value;
+    let password = event.target.password.value;
+
+    AthletesAdapter.getAthletes().then(json => {
+      console.log(json);
+      for (const athlete of json.data) {
+        // if the username and password match a specific user
+        if (
+          athlete.attributes.username === username &&
+          athlete.attributes.password_digest === password
+        ) {
+          // set the currentUser to the athlete
+          currentUser = athlete;
+          // stop execution
+          return;
+        }
+      }
+      // if we have NOT found an athlete, we'll send out a message.
+      if (!currentUser) {
+        // send out errors
+        alert('Sorry, incorrect username or password!');
+      }
+    });
+
+    // fetch the athlete (find_by)
+
+    // alert('Logging in')
   }
 }
 
 class RegistrationForm {
   constructor() {
-  this.currentPage = document.querySelector('.registration-page');
-  this.registrationFormContainer = document.createElement('div');
-  this.currentPage.append(this.registrationFormContainer);
-  this.registrationFormContainer.classList.add('registration-form-container');
-  this.registrationFormContainer.innerHTML = 
-  `
+    this.currentPage = document.querySelector('.registration-page');
+    this.registrationFormContainer = document.createElement('div');
+    this.currentPage.append(this.registrationFormContainer);
+    this.registrationFormContainer.classList.add('registration-form-container');
+    this.registrationFormContainer.innerHTML = `
   <form class="registration-form" method="get">
     Username: <input type="text" name="username"><br>
     Password: <input type="text" name="password"><br>
@@ -90,11 +117,11 @@ class RegistrationForm {
     Confirm Password: <input type="text" name="Email"><br>
     <input type="submit" value="Submit">
   </form>
-  `
-  this.registrationForm = document.querySelector('.registration-form')
-  this.registrationForm.addEventListener('submit', event => {
-    event.preventDefault()
-    alert('registering now')
-  })
+  `;
+    this.registrationForm = document.querySelector('.registration-form');
+    this.registrationForm.addEventListener('submit', event => {
+      event.preventDefault();
+      alert('registering now');
+    });
   }
 }
